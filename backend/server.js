268fc4a8568e50fc40f0bb45efa2e23ba44173bc
rodrigo pichardo <hectorrodrigo2017@gmail.com>
2021-09-26 10:17:45 -0500

    comentar express.static

--- backend/server.js
+++ backend/server.js
@@ -26,14 +26,14 @@ app.get('/api/staff', (req, res) => {
   res.send(data.staff);
 });
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
-});
-
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
 // });
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
