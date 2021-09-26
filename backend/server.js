06f4fb251d46044559d944ece60b24362a16b518
rodrigo pichardo <hectorrodrigo2017@gmail.com>
2021-09-26 11:10:53 -0500

    /characters

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,6 @@ import data from './data.js';
 
 const app = express();
 
-// Necesito una api para traer los detalles del character
-app.get('/api/characters/:id', (req, res) => {
-  // buscaremos el producto
-  const character = data.characters.find((x) => x._id === req.params.id);
-  if (character) {
-    res.send(character);
-  } else {
-    res.status(404).send({ message: 'Caracter not Found' });
-  }
-});
-
 app.get('/api/characters', (req, res) => {
   res.send(data.characters);
 });
@@ -26,8 +15,19 @@ app.get('/api/staff', (req, res) => {
   res.send(data.staff);
 });
 
+// Necesito una api para traer los detalles del character
+app.get('/api/characters/:id', (req, res) => {
+  // buscaremos el producto
+  const character = data.characters.find((x) => x._id === req.params.id);
+  if (character) {
+    res.send(character);
+  } else {
+    res.status(404).send({ message: 'Caracter not Found' });
+  }
+});
+
 const __dirname = path.resolve();
-app.use('/', express.static(path.join(__dirname, '/')));
+app.use('/characters', express.static(path.join(__dirname, '/characters')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
